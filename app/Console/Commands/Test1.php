a57629af28ac3fe5b83c3281a4f258aad413214b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-07-26 22:56:04 -0300

    Writing Output

---
+++ app/Console/Commands/Test1.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+/**
+ * @see https://laravel.com/docs/8.x/artisan#writing-output
+ */
+class Test1 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'jean:test1';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Testando escritas na tela';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->line('Executando o método ' . __METHOD__);
+        $this->newLine();
+
+        $this->line('Escrevendo uma linha 1');
+        $this->line('Escrevendo uma linha 2');
+        $this->newLine();
+
+        $this->info('Escrever string como saída de informação');
+        $this->newLine();
+
+        $this->comment('Escrever string como saída de comentário');
+        $this->newLine();
+
+        $this->question('Escrever string como saída da pergunta');
+        $this->newLine();
+
+        $this->error('Escrever string como saída de erro');
+        $this->newLine();
+
+        $this->warn('Escrever string como saída de aviso');
+        $this->newLine();
+
+        $this->alert('Escrever string em uma caixa de alerta');
+        $this->newLine();
+    }
+}
